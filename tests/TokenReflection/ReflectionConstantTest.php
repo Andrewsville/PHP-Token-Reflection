f49c49eeea8da2896f8ec00d58fad0040fdce2d9
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-06-19 15:00:35 +0200

    Added license to tests

--- tests/TokenReflection/ReflectionConstantTest.php
+++ tests/TokenReflection/ReflectionConstantTest.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * PHP Token Reflection
+ *
+ * Version 1.0 beta 2
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this library in the file LICENSE.
+ *
+ * @author Ondřej Nešpor <andrew@andrewsville.cz>
+ * @author Jaroslav Hanslík <kukulich@kukulich.cz>
+ */
 
 namespace TokenReflection;
 
