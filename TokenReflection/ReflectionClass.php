76a9f83395b99a5e6350471af5858a8cd2650c72
Ondrej Nespor <andrew@andrewsville.cz>
2012-01-29 23:22:33 +0100

    PHP 5.4 internal reflection compatibility

--- TokenReflection/ReflectionClass.php
+++ TokenReflection/ReflectionClass.php
@@ -1467,9 +1467,6 @@ class ReflectionClass extends ReflectionElement implements IReflectionClass
 			}
 			// Indent
 			$string = "\n    ";
-			if (null !== $method->getDeclaringTraitName()) {
-				$string .= "\n    ";
-			}
 
 			$string .= preg_replace('~\n(?!$|\n|\s*\*)~', "\n    ", $method->__toString());
 			// Add inherits
