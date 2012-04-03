353ab963822e7a41f0db75cf4911083d5f66ce08
Ondrej Nespor <andrew@andrewsville.cz>
2012-04-04 01:38:20 +0200

    Better constant/property/parameter value evaluation

--- TokenReflection/Resolver.php
+++ TokenReflection/Resolver.php
@@ -207,7 +207,7 @@ class Resolver
 			}
 		}
 
-		return eval(sprintf('return %s;', $source));
+		return self::evaluate(sprintf("return %s;\n", $source));
 	}
 
 	/**
@@ -274,5 +274,15 @@ class Resolver
 			}
 		}
 		return $constants;
+	}
+
+	/**
+	 * Evaluates a source code.
+	 *
+	 * @param string $source Source code
+	 * @return mixed
+	 */
+	final private static function evaluate($source) {
+		return eval($source);
 	}
 }
